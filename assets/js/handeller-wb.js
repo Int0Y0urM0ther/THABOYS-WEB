6d0bf68f6010803ffa15bfb9d3502b08743ba7a5
Int0Y0urM0ther <gnooby436@gmail.com>
2024-11-28 13:33:23 +0100

    Add files via upload

---
+++ assets/js/handeller-wb.js
@@ -0,0 +1,12 @@
+function downloadwb(string) {
+    let version = string;
+    if(version == '1') {
+        window.open('https://downgit.evecalm.com/#/home?url=https://github.com/OneBoyOG/THABOYS-WEB/blob/main/DcWHSpmer.bat', '_blank');
+    } else if(version == '2') {
+        window.open('https://downgit.evecalm.com/#/home?url=https://github.com/OneBoyOG/THABOYS-WEB/blob/main/DcWHSpmer.bat', '_blank');
+    } else if(version == '3') {
+        window.open('https://downgit.evecalm.com/#/home?url=https://github.com/OneBoyOG/THABOYS-WEB/blob/main/DcWHSpmer.bat', '_blank');
+    } else {
+        window.open('/download/404.html', '_blank');
+    }
+}
---
+++ assets/js/handeller.js
@@ -0,0 +1,12 @@
+function download(string) {
+    let item = string;
+    if(item == 'dchb') {
+        window.open('/download/discord-webhock-spammer.html', '_blank');
+    } else if(item == 'ddos') {
+        window.open('/download/ddos.html', '_blank');
+    } else if(item == 'terminal') {
+        window.open('/download/terminal.html', '_blank');
+    } else {
+        window.open('/download/404.html', '_blank');
+    }
+}
